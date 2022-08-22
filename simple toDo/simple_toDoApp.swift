da87f015fd683968b43e84a0f0f3822d34c31323
Nikita-Ananev <74005578+Nikita-Ananev@users.noreply.github.com>
2022-08-22 18:12:24 +0300

    create app

---
+++ simple toDo/simple_toDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_toDoApp.swift
+//  simple toDo
+//
+//  Created by Никита Ананьев on 22.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct simple_toDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
